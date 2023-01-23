a48c4e8a8e8aaacbc32f9f341cc730c3f9806195
Doorbals <rbals980@gmail.com>
2023-01-24 01:11:34 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.cpp
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.cpp
@@ -9,7 +9,6 @@ vector<int> skillCheck(26, -1);
 int solution(string skill, vector<string> skill_trees)
 {
     int count = skill_trees.size();
-    
 
     for (string str : skill_trees)
     {
@@ -18,7 +17,6 @@ int solution(string skill, vector<string> skill_trees)
         for (char ch : str)
         {
             bool correct = true;
-            
             for (int i = 0; i < skill.size(); i++)
             {
                 if ((skill[i] != ch) && (skillCheck[skill[i] - 'A'] == 0) && (skillCheck[ch - 'A'] != -1))
@@ -38,6 +36,5 @@ int solution(string skill, vector<string> skill_trees)
         }
         skillCheck.assign(26, -1);
     }
-
     return count;
 }
