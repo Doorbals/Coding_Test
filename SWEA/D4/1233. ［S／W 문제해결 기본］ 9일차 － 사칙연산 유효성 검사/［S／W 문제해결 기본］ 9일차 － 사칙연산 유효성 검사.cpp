ff2fd4c0af704ba904e9f1be58cbb89a8dd0b784
Doorbals <rbals980@gmail.com>
2023-01-26 22:57:01 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 9 ms, Memory: 13,560 KB -BaekjoonHub

---
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <list>
+#include <sstream>
+#include <string>
+
+using namespace std;	
+typedef tuple<int, char, int, int> ticii;
+
+vector<ticii> nodes;
+char currentCh = '0';
+bool isRight = true;
+
+vector<string> split(string input, char dlim)
+{
+	vector<string> result;
+
+	stringstream ss;
+	string stringBuffer;
+	ss.str(input);
+
+	while (getline(ss, stringBuffer, dlim))
+	{
+		result.push_back(stringBuffer);
+	}
+
+	return result;
+}
+
+void inOrder(int currentNode)
+{
+	int left = get<2>(nodes[currentNode]);
+	int right = get<3>(nodes[currentNode]);
+	
+	if (left != -1) inOrder(left);
+	if (!isdigit(currentCh) && !isdigit(get<1>(nodes[currentNode])))
+	{
+		isRight = false;
+		return;
+	}
+	else
+		currentCh = get<1>(nodes[currentNode]);
+
+	if (right != -1) inOrder(right);
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(); cout.tie();
+
+	for (int t = 0; t < 10; t++)
+	{
+		string nStr;
+		getline(cin, nStr);
+		int n = stoi(nStr);
+
+		for (int i = 0; i < n; i++)
+		{
+			string str;
+			getline(cin, str);
+			vector<string> splited = split(str, ' ');
+
+			int nodeNum, left = 0, right = 0;
+			char ch;
+			if (splited.size() > 1)
+			{
+				nodeNum = stoi(splited[0]);
+				ch = splited[1][0];
+				if (splited.size() > 2)
+				{
+					left = stoi(splited[2]);
+					if (splited.size() > 3)
+						right = stoi(splited[3]);
+				}
+			}
+			nodes.push_back(ticii(nodeNum, ch, left - 1, right - 1));
+		}
+		cout << '#' << t + 1 << ' ';
+		inOrder(0);
+		cout << (isRight ? 1 : 0) << endl;
+
+		nodes.clear();
+		currentCh = '0';
+		isRight = true;
+	}
+}
