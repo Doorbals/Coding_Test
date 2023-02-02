2feb88c07ebbc572adc05ec7f9c60e9f9360f32e
Doorbals <rbals980@gmail.com>
2023-02-03 01:48:42 +0900

    [Gold IV] Title: 네트워크 연결, Time: 32 ms, Memory: 6556 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <queue>
+
+using namespace std;
+typedef pair<int, int> pii;
+
+vector<vector<pii>> edges;
+bool mst[1001];
+int minCost = 0;
+
+void prim(int n)
+{
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+	int i = 1, count = 1;
+
+	while (true)
+	{
+		mst[i] = true;
+
+		for (int j = 0; j < edges[i].size(); j++)
+		{
+			int curNode = edges[i][j].second;
+			if (!mst[curNode])
+				pq.push(edges[i][j]);
+		}
+
+		for (int k = 0; k < pq.size(); k++)
+		{
+			pii curEdge = pq.top();
+			pq.pop();
+			if (!mst[curEdge.second])
+			{
+				i = curEdge.second;
+				minCost += curEdge.first;
+				count++;
+				break;
+			}	
+		}
+		if (count == n)
+			return;
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(); cout.tie();
+
+	int n, m;
+	cin >> n >> m;
+
+	edges.resize(n * 2);
+	for (int i = 0; i < m; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		
+		edges[a].push_back(pii(c, b));
+		edges[b].push_back(pii(c, a));
+	}
+	prim(n);
+	cout << minCost;
+}
