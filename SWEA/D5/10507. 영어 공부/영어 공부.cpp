12e6a0cb9984db8885b29cb917f311b31c43aebe
Doorbals <rbals980@gmail.com>
2023-02-09 01:34:38 +0900

    [D5] Title: 영어 공부, Time: 436 ms, Memory: 17,524 KB -BaekjoonHub

--- SWEA/D5/10507. 영어 공부/영어 공부.cpp
+++ SWEA/D5/10507. 영어 공부/영어 공부.cpp
@@ -1,32 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <tuple>
+#include <queue>
+#include <cmath>
 
 using namespace std;
 int days[200001];
 int maxPeriod;
-vector<int> blanks;
+vector<int> blanks;		// days의 첫 날짜부터 다른 날짜까지 공백(문제 안 푼 날)을 저장 (ex. [1]에는 days[0] ~ days[1] 사이의 안 푼 일수) 
 
 int binary_search(int n, int p)
 {
-	vector<int> answers;
-	for (int k = 0; k < n; k++)
+	vector<int> answers;	// 각 날짜에서 시작했을 때 가장 긴 연속 기간을 저장
+
+	// days[i] 날짜부터 시작했을 때 가장 긴 연속 기간을 이진 탐색으로 구한다.
+	for (int i = 0; i < n; i++)
 	{
-		int start = k, end = n - 1;
+		int start = i, end = n - 1;
 		int answer = 0;
 		while (end >= start)
 		{
 			int mid = start + (end - start) / 2;
-			int didntStudy = 0, curP = p;
-			didntStudy = blanks[mid] - blanks[k];
+			int didntStudy = 0, curP = p;			// didntStudy : days[i] 날짜에서 days[mid] 날짜까지 중 공백(공부 안 한 일수) / curP : days[i]에서 days[mid]까지 연속시켰을 때 남는 p
+			didntStudy = blanks[mid] - blanks[i];
 
-			curP = (p - didntStudy > 0) ? p - didntStudy : 0;
+			curP = (p - didntStudy > 0) ? p - didntStudy : 0;		// 남은 p가 음수면 0으로 처리
 
-			if (didntStudy > p)
+			if (didntStudy > p)			// 추가로 체크할 수 있는 날보다 공백 개수가 더 많음 -> days[mid] 이상의 날짜는 불가능하므로 그 아래 값을 탐색
 				end = mid - 1;
-			else
+			else                        // 추가로 체크할 수 있는 날을 딱 맞춰 사용했거나, 더 적게 사용 -> days[i] ~ days[mid]일 때의 연속 기간을 answer로 설정해두고, 그 위 값도 탐색
 			{
-				answer = days[mid] - days[k] + 1 + curP;
+				answer = days[mid] - days[i] + 1 + curP;
 				start = mid + 1;
 			}
 		}
@@ -34,7 +39,6 @@ int binary_search(int n, int p)
 		if (maxPeriod < answer)
 			maxPeriod = answer;
 	}
-
 	return maxPeriod;
 }
 
@@ -61,8 +65,10 @@ int main()
 
 		int blankCount = 0;
 		blanks.clear();
-		blanks.push_back(0);
-		for (int j = 0; j < n - 1; j++)
+		blanks.push_back(0);				// 맨 첫 날짜까지는 공백 X
+
+		// blankCount에 현재 순서 날짜와 다음 순서 날짜 사이의 공백 누적하고 이를 blanks에 삽입 
+		for (int j = 0; j < n - 1; j++)		
 		{
 			blankCount += days[j + 1] - days[j] - 1;
 			blanks.push_back(blankCount);
