4c4e4f1ee5b16e9480fb13ab6b71858e167c616b
Doorbals <rbals980@gmail.com>
2023-02-20 20:27:29 +0900

    [Silver I] Title: 격자상의 경로, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.cc
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.cc
@@ -3,8 +3,8 @@
 #include <algorithm>
 
 using namespace std;
-int dp[16][16];
-int dp2[16][16];
+int dp[16][16];		
+int dp2[16][16];	
 int n, m, k;
 
 int solution()
@@ -15,44 +15,26 @@ int solution()
 	for (int i = 0; i < n; i++)
 		dp[i][0] = 1;
 
-	if (k == 0)
+	int stopOverY = k / m, stopOverX = k % m;
+
+	for (int i = 1; i <= stopOverY; i++)
 	{
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = 1; j <= m; j++)
-			{
-				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-			}
-		}
-		return dp[n - 1][m - 1];
+		for (int j = 1; j <= stopOverX; j++)
+			dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
 	}
-	else
+
+	for (int i = stopOverX; i < m; i++)
+		dp2[stopOverY][i] = 1;
+
+	for (int i = stopOverY; i < n; i++)
+		dp2[i][stopOverX] = 1;
+
+	for (int i = stopOverY + 1; i < n; i++)
 	{
-		int stopOverY = k / m, stopOverX = k % m - 1;
-
-		for (int i = 1; i <= stopOverY; i++)
-		{
-			for (int j = 1; j <= stopOverX; j++)
-			{
-				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-			}
-		}
-
-		for (int i = stopOverX; i < m; i++)
-			dp2[stopOverY][i] = 1;
-
-		for (int i = stopOverY; i < n; i++)
-			dp2[i][stopOverX] = 1;
-
-		for (int i = stopOverY + 1; i < n; i++)
-		{
-			for (int j = stopOverX + 1; j < m; j++)
-			{
-				dp2[i][j] = dp2[i - 1][j] + dp2[i][j - 1];
-			}
-		}
-		return dp[stopOverY][stopOverX] * dp2[n - 1][m - 1];
+		for (int j = stopOverX + 1; j < m; j++)
+			dp2[i][j] = dp2[i - 1][j] + dp2[i][j - 1];
 	}
+	return dp[stopOverY][stopOverX] * dp2[n - 1][m - 1];
 }
 
 int main()
@@ -61,5 +43,6 @@ int main()
 	cin.tie(nullptr); cout.tie(nullptr);
 
 	cin >> n >> m >> k;
+	k = k - 1;		
 	cout << solution();
 }
