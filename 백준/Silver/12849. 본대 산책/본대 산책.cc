1774ea8905d2b0eacb080377b31ab22d4af2d4a6
Doorbals <rbals980@gmail.com>
2023-02-24 00:34:22 +0900

    [Silver I] Title: 본대 산책, Time: 4 ms, Memory: 8272 KB -BaekjoonHub

--- 백준/Silver/12849. 본대 산책/본대 산책.cc
+++ 백준/Silver/12849. 본대 산책/본대 산책.cc
@@ -1,23 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <tuple>
-#include <queue>
-#include <cmath>
 
 using namespace std;
 
-long long dp[8][100001];			// dp[i][j] : i번 건물에 j분에 도착하는 경우의 수
+long long dp[8][100001];			
 const long long MOD = 1000000007;
 int n;
 
-// 정보과학관 : 0 / 전산관 : 1 / 미래관 : 2 / 신양관 : 3 / 한경직기념관 : 4 / 진리관 : 5 / 학생회관 : 6 / 형남공학관 : 7
 long long solution()
 {
 	dp[0][0] = 1;
 
-	// i번 건물에 j분에 도착하는 경우의 수는 (i번 건물에 연결된 건물들에 i-1분에 도착하는 경우의 수의 합)
-	for (int i = 1; i <= n + 1; i++)
+	for (int i = 1; i <= n; i++)
 	{
 		dp[0][i] = (dp[1][i - 1] + dp[2][i - 1]) % MOD;
 		dp[1][i] = (dp[0][i - 1] + dp[2][i - 1] + dp[3][i - 1]) % MOD;
