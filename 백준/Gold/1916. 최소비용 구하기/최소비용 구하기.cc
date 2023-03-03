c337c7ec9478e0fe4935a99640a9af14b07a5cf8
Doorbals <rbals980@gmail.com>
2023-03-04 01:59:59 +0900

    [Gold V] Title: 최소비용 구하기, Time: 36 ms, Memory: 4748 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+typedef pair<int, int> pii;
+
+int n, m, start, destination;
+vector<vector<pii>> graph;
+int dp[1001];
+const long long INF = 1e9
+;
+void dijkstra(int start)
+{
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+	pq.push(pii(0, start));
+	dp[start] = 0;
+
+	while (!pq.empty())
+	{
+		int dist = pq.top().first;
+		int cur = pq.top().second;
+		pq.pop();
+		if (dp[cur] < dist) continue;
+		for (int i = 0; i < graph[cur].size(); i++)
+		{
+			int cost = dist + graph[cur][i].second;
+			if (cost < dp[graph[cur][i].first])
+			{
+				dp[graph[cur][i].first] = cost;
+				pq.push(pii(cost, graph[cur][i].first));
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	cin >> n >> m;
+
+	graph.resize(n + 1);
+	for (int i = 0; i < m; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		graph[a].push_back(pii(b, c));
+	}
+
+	cin >> start >> destination;
+	fill(dp, dp + n + 1, INF);
+	dijkstra(start);
+
+	cout << dp[destination];
+}
