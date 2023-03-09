60aaa102138f404ca5e8b611c140fa9efffbd8b1
Doorbals <rbals980@gmail.com>
2023-03-10 01:45:40 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -8,13 +8,15 @@
 #include <list>
 using namespace std;
 typedef pair<int, int> pii;
+
 vector<vector<pii>> graph;
+vector<vector<pii>> reverse_graph;
 int dp[1001];
 int result[1001];
 int n, m, x;
 const long long INF = 1e9;
 
-void dijkstra(int start)
+void dijkstra(int start, bool reverse)
 {
 	priority_queue<pii, vector<pii>, greater<pii>> pq;
 	pq.push(pii(0, start));
@@ -25,14 +27,30 @@ void dijkstra(int start)
 		int cur = pq.top().second;
 		pq.pop();
 		if (dp[cur] < dist)	continue;
-		for (int i = 0; i < graph[cur].size(); i++)
+		if (!reverse)
 		{
-			pii curEdge = graph[cur][i];
-			int cost = dist + curEdge.first;
-			if (cost < dp[curEdge.second])
+			for (int i = 0; i < graph[cur].size(); i++)
 			{
-				dp[curEdge.second] = cost;
-				pq.push(pii(cost, curEdge.second));
+				pii curEdge = graph[cur][i];
+				int cost = dist + curEdge.first;
+				if (cost < dp[curEdge.second])
+				{
+					dp[curEdge.second] = cost;
+					pq.push(pii(cost, curEdge.second));
+				}
+			}
+		}
+		else
+		{
+			for (int i = 0; i < reverse_graph[cur].size(); i++)
+			{
+				pii curEdge = reverse_graph[cur][i];
+				int cost = dist + curEdge.first;
+				if (cost < dp[curEdge.second])
+				{
+					dp[curEdge.second] = cost;
+					pq.push(pii(cost, curEdge.second));
+				}
 			}
 		}
 	}
@@ -44,22 +62,24 @@ int main()
 	cin.tie(nullptr); cout.tie(nullptr);
 	cin >> n >> m >> x;
 	graph.resize(n + 1);
+	reverse_graph.resize(n + 1);
 	for (int i = 0; i < m; i++)
 	{
 		int a, b, c;
 		cin >> a >> b >> c;
 		graph[a].push_back(pii(c, b));
-	}
-	
-	for (int i = 1; i <= n; i++)
-	{
-		fill(dp, dp + n + 1, INF);
-		dijkstra(i);
-		result[i] += dp[x];
+		reverse_graph[b].push_back(pii(c, a));
 	}
 
+	// 1 ~ n 도시부터 x 도시까지의 거리 구하기
 	fill(dp, dp + n + 1, INF);
-	dijkstra(x);
+	dijkstra(x, true);
+	for (int i = 1; i <= n; i++)
+		result[i] += dp[i];
+
+	// x 도시부터 1 ~ n 도시까지의 거리 구하기
+	fill(dp, dp + n + 1, INF);
+	dijkstra(x, false);
 	for (int i = 1; i <= n; i++)
 		result[i] += dp[i];
 
