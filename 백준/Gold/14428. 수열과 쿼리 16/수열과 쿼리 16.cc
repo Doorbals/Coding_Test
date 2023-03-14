211513aa02c383e1a105667ba7487857d56d1d4f
Doorbals <rbals980@gmail.com>
2023-03-14 12:28:42 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 84 ms, Memory: 9052 KB -BaekjoonHub

---
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef pair<int, long long> pli;
+
+pli tree[400004];
+long long datas[100001];
+const long long INF = 1e9 + 1;
+int n, m;
+
+pli init(int start, int end, int node)
+{
+	if (start == end) return tree[node] = {datas[start], start};
+	int mid = start + (end - start) / 2; 
+	pli a = init(start, mid, node * 2);
+	pli b = init(mid + 1, end, node * 2 + 1);
+	return tree[node] = min(a, b);
+}
+
+pli findMin(int start, int end, int node, int left, int right)
+{
+	if (end < left || right < start) return { INF, 0 };
+	if (left <= start && end <= right) return tree[node];
+	int mid = start + (end - start) / 2;
+	return min(findMin(start, mid, node * 2, left, right),
+		findMin(mid + 1, end, node * 2 + 1, left, right));
+}
+
+pli update(int start, int end, int node, int index, long long dif)
+{
+	if (index < start || end < index) return tree[node];
+	if (start == end) return tree[node] = { dif, tree[node].second };
+	int mid = start + (end - start) / 2;
+	return tree[node] = min(update(start, mid, node * 2, index, dif),
+		update(mid + 1, end, node * 2 + 1, index, dif));
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> datas[i];
+
+	init(1, n, 1);
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		if (a == 1)
+		{
+			update(1, n, 1, b, c);
+			datas[b] = c;
+		}
+		else
+			cout << findMin(1, n, 1, b, c).second << '\n';
+	}
+}
