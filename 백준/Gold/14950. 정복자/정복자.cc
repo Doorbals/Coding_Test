e92d1c624a327415e264356a46521155f2893542
Gyumin Moon <rbals980@gmail.com>
2023-07-16 14:06:02 +0900

    [Gold III] Title: 정복자, Time: 12 ms, Memory: 3344 KB -BaekjoonHub

--- 백준/Gold/14950. 정복자/정복자.cc
+++ 백준/Gold/14950. 정복자/정복자.cc
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 #include <queue>
 #include <tuple>
+
 using namespace std;
 typedef tuple<int, int, int> tiii;
 
-int n, m, t;
-vector<tiii> edges;
 int parent[10001];
+vector<tiii> edges;
 
 int getParent(int n)
 {
-	if (parent[n] == n)
-		return n;
-	else
-		return parent[n] = getParent(parent[n]);
+	if (parent[n] == n) return n;
+	return parent[n] = getParent(parent[n]);
 }
 
 void unionParent(int a, int b)
 {
-	a = getParent(a);
+	a = getParent(a); 
 	b = getParent(b);
 
 	if (a < b)
@@ -42,7 +41,9 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr); cout.tie(nullptr);
 
+	int n, m, t;
 	cin >> n >> m >> t;
+
 	for (int i = 0; i < m; i++)
 	{
 		int a, b, c;
@@ -54,22 +55,20 @@ int main()
 	sort(edges.begin(), edges.end());
 	for (int i = 1; i <= n; i++)
 		parent[i] = i;
-	
+
 	int sum = 0;
 	int count = 0;
 	for (int i = 0; i < edges.size(); i++)
 	{
 		int curA = get<1>(edges[i]);
 		int curB = get<2>(edges[i]);
-
+	
 		if (!findParent(curA, curB))
 		{
 			unionParent(curA, curB);
-			sum += get<0>(edges[i]);
-			sum += t * count;
+			sum += (count * t) + get<0>(edges[i]);
 			count++;
 		}
 	}
-
 	cout << sum;
 }
