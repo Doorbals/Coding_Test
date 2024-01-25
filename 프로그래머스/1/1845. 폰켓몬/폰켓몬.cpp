cc298ee3869c49c5273e3d97dc1bf7f87839cb9a
Gyumin Moon <rbals980@gmail.com>
2024-01-25 19:46:37 +0900

    [level 1] Title: 폰켓몬, Time: 0.15 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,12 +1,13 @@
 #include <vector>
 #include <set>
+#include <unordered_set>
 
 using namespace std;
 
 int solution(vector<int> nums)
 {
     int max = nums.size() / 2;
-    set<int> s(nums.begin(), nums.end());
+    unordered_set<int> s(nums.begin(), nums.end());
 
     int size = s.size();
     return max < size ? max : size;
