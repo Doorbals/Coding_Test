c04f5d3615620fb9042f0ce850f6c3b155819831
Gyumin Moon <rbals980@gmail.com>
2024-01-25 19:41:52 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    int max = nums.size() / 2;
+    set<int> s;
+    
+    for(int i = 0; i < nums.size(); i++)
+    {
+        s.insert(nums[i]);    
+    }
+    
+    int size = s.size();
+    
+    return max < size ? max : size;
+}
