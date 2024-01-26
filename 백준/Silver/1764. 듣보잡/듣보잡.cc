7cf74198bf0f0a55bdde5b99c95d9ff4667a2d84
Gyumin Moon <rbals980@gmail.com>
2024-01-26 23:49:18 +0900

    [Silver IV] Title: 듣보잡, Time: 416 ms, Memory: 7580 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int n, m;
+	cin >> n >> m;
+	set<string> s;
+	vector<string> answer;
+
+	for (int i = 0; i < n; i++)
+	{
+		string name;
+		cin >> name;
+		s.insert(name);
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		string name;
+		cin >> name;
+		set<string>::iterator findIt = s.find(name);
+		if (findIt != s.end())
+		{
+			answer.push_back(name);
+		}
+	}
+
+	sort(answer.begin(), answer.end());
+	cout << answer.size() << endl;
+	for (int i = 0; i < answer.size(); i++)
+	{
+		cout << answer[i] << endl;
+	}
+}
