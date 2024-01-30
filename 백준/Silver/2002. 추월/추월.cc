aa34a1ed0a0fdca5b21195834bdd4fe99104e2a3
Gyumin Moon <rbals980@gmail.com>
2024-01-30 18:27:14 +0900

    [Silver I] Title: 추월, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/2002. 추월/추월.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<string, int> cars;
+vector<bool> check;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	
+	int n, count;
+	cin >> n;
+	count = 0;
+
+	string inCarNumber;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> inCarNumber;
+		cars[inCarNumber] = i;
+		check.push_back(false);
+	}
+
+	string outCarNumber;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> outCarNumber;
+		int cur = cars[outCarNumber];
+
+		bool result = true;
+		for (int j = 0; j < cur; j++)
+		{
+			if (!check[j])
+			{
+				result = false;
+				break;
+			}
+		}
+		
+		if(!result)
+		{
+			count++;
+		}
+
+		check[cur] = true;
+	}
+	
+	cout << count;
+	return 0;
+}
